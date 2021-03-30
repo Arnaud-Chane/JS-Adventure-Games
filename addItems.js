c515f19504a1a9895a4117e55e3b9affbcedd0aa
Arnaud-Chane <59416180+Arnaud-Chane@users.noreply.github.com>
2021-03-30 22:57:30 +0200

    Add files via upload

---
+++ addItems.js
@@ -0,0 +1,7 @@
+let inventory = {};
+inventory.w1 = prompt("Which weapon ?");
+alert(inventory.w1);
+inventory.w2 = prompt("Which weapon ?");
+alert(inventory.w2);
+inventory.w3 = prompt("Which weapon ?");
+alert(inventory.w1);
